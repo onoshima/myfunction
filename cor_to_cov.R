307ebfbfb017e9366255c0393d1a735ee4d8a510
onoshima <onoshima.t@gmail.com>
2020-02-04 17:03:36 +0900

    相関係数行列から分散共分散行列を作る関数を追加

---
+++ cor_to_cov.R
@@ -0,0 +1,13 @@
+cor_to_cov <- function(R, SD){
+  if (nrow(R)!=ncol(R)){
+    stop("The input matrix is not squared.")
+  } else if (nrow(R)!=length(SD)){
+    stop("The number of variables of correlation matrix and SD vector are dirfferent.")
+  }
+  num_var <- nrow(R)
+  D <- diag(SDs)
+  V <- D %*% R %*% D
+  rownames(V) <- rownames(R)
+  colnames(V) <- colnames(R)
+  return(V)
+}
