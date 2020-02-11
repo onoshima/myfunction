e118defd4375120bf71869f9f7f2764371475de3
onoshima <onoshima.t@gmail.com>
2020-02-11 17:02:23 +0900

    下三角行列を行列にする関数に対角成分を含む機能を追加

--- tri_to_matrix.R
+++ tri_to_matrix.R
@@ -1,9 +1,19 @@
-tri_to_matrix <- function(x){
-  num_datapoints <- length(x)
-  num_orders <- sqrt((2*num_datapoints+0.25)) + 0.5
-  cormatrix <- diag(0,num_orders)
-  cormatrix[lower.tri(cormatrix)] <- x
-  cormatrix <- cormatrix + t(cormatrix)
-  diag(cormatrix) <- 1
-  return(cormatrix)
+tri_to_matrix <- function(x, include.diag=F){
+  if (include.diag){
+    num_datapoints <- length(x)
+    num_orders <- sqrt((2*num_datapoints+0.25)) - 0.5
+  } else {
+    num_datapoints <- length(x)
+    num_orders <- sqrt((2*num_datapoints+0.25)) + 0.5
+  }
+  covmatrix <- diag(0,num_orders)
+  if (include.diag){
+    covmatrix[lower.tri(covmatrix, diag=T)] <- x
+  } else {
+    covmatrix[lower.tri(covmatrix)] <- x
+    diag(covmatrix) <- 1
+  }
+  covmatrix <- covmatrix + t(covmatrix)
+  diag(covmatrix) <- diag(covmatrix)/2
+  return(covmatrix)
 }
